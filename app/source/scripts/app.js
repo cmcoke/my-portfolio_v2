299a30fd467e2c1eb4eb763e5580982400cacfb4
cmcoke <minoru.cm@gmail.com>
2019-06-23 22:11:13 -0500

    created the projects section

---
+++ app/source/scripts/app.js
@@ -0,0 +1,12 @@
+    // /*===========Portfolio isotope js===========*/
+    // var item = document.querySelector('.portfolio_single_wrap');
+    // var iso = new Isotope (item, {
+    //     itemSelector: '.portfolio_single_item',
+    //     percentPosition: true,
+    //     layoutMode: 'masonry',
+    //     masonry: {
+    //         // use outer width of grid-sizer for columnWidth
+    //         columnWidth: 1
+    //     }
+    // });
+
